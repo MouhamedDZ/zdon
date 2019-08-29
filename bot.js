ad997e9081a39de24286fad66b8672926ef485b6
MouhamedDZ <54417600+MouhamedDZ@users.noreply.github.com>
2019-08-29 02:56:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "ID حقك") return;
+  if (message.author.id !== "522306249451110400") return;
 
   
   if (message.content.startsWith(prefix + 'setw')) {
